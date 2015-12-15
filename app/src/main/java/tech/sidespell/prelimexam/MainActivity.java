e7b7c07d2a0c9819745db85865514b6e13339252
lendale <nasibag1>
2015-12-15 16:57:35 +0800

    -Changed the text values of the views. -Changed the id's of the views. -Founded all the views. -Instantiated a Handler -Instantiated a Runnable

--- app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
+++ app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
@@ -1,12 +1,27 @@
 package tech.sidespell.prelimexam;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private static final String TAG = "";
+    private ToggleButton btnSwitch;
+    private TextView tvCounter, tvSpeed;
+    private SeekBar seekBarSpeed;
+    private RadioButton rdBtnInc, rdBtnDec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +30,27 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Finding all views
+        btnSwitch = (ToggleButton) findViewById(R.id.toggleButton);
+        tvCounter = (TextView) findViewById(R.id.tvCounter);
+        tvSpeed = (TextView) findViewById(R.id.tvSpeed);
+        seekBarSpeed = (SeekBar) findViewById(R.id.seekBar);
+        rdBtnInc = (RadioButton) findViewById(R.id.radioButtonInc);
+        rdBtnDec = (RadioButton) findViewById(R.id.radioButtonDec);
+
+        //Ser the appropriate listeners
+        btnSwitch.setOnCheckedChangeListener(this);
+
+        final Handler handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+
     }
 
     @Override
@@ -37,5 +73,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            Log.d(TAG, "onCheckedChanged: STOP");
+        } else {
+            Log.d(TAG, "onCheckedChanged: START");
+        }
     }
 }
